fb8ba1733857c6fb1b221d50ee25860a05277556
Arebelly Akshay <121184687+Arebelly-Akshay12222418@users.noreply.github.com>
2024-07-01 15:30:33 +0530

    App.js

--- module 2/Lecture19_Listening_To_Events/App.js
+++ module 2/Lecture19_Listening_To_Events/App.js
@@ -1,1 +1,15 @@
+import { Component } from "react";
+import ToDoScreen from "./screen/ToDoScreen";
 
+
+class App extends Component{
+    render(){
+        return(
+            <>
+                <ToDoScreen/>
+            </>
+        );
+    }
+}
+
+export default App;
